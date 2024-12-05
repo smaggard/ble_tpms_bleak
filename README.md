451d9c513dd02aaea1c53f7ce8af0edf42128860
Stephen Maggard <smaggard@me.com>
2024-12-05 07:50:11 -0600

    Cleaning up script and adding to README

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # ble_tpms_bleak
-TPMS system that will send messages to a Holley Terminator X or Dominator as a Can I/O module
+TPMS system that will send messages to a Holley Terminator X or Dominator as a Can I/O module.
+
+This application is written to be ran on a Raspberry Pi (Pi4 8GB used for Testing), along with 
+CAN Hat https://www.amazon.com/gp/product/B07VMB1ZKH and TPMS sensors https://www.amazon.com/dp/B0DBPLD1MQ
+
+After installing the sensors on the tires you will directly after need to run the following command on the 
+raspberry pi in close proximity to the car.
+```
+bluetoothclt
+scan le
+```
+Then watch for the 4 devices that start with 80, 81, 82 and 83, those sensors will align to sensor 1,2,3,and 4 respectively.
+Enter those id's as the keys for devices_dict dictionary in tpms.py. then stop scanning and exit by running 
+```
+scan off
+exit
+```
 
 # Installation
-## Install required modules.
 ## Modify tpms.py file.
 Edit tpms.py file and change the Keys of the devices dict to match your TPMS sensors.
 ## Copy files to directoryies
@@ -23,4 +38,35 @@ Start Service
 ```systemctl enable tpms```
 
 # Holley EFI Setup
-TODO
+Each wheel TPMS will use up to 3 inputs, its up to you how many of them you use.
+The inputs will be Pressure in PSI, Temperature in degrees F, and the Battery %
+
+## Holley input can ids are as follows.
+### For pressure
+Configure as CAN 5V Sensors with a Type of Custom 5v, the voltage scale will be 0-5v, and the Pressure scale will be 0-50PSI.
+Use the CAN IDs and input numbers from the list below.
+
+*Can I/O Module ID 1575
+** Input 1 = Driver Front
+** Input 2 = Passenger Front
+** Input 3 = Driver Rear
+** Input 4 = Passenger Rear
+
+### For Temperature
+Configure as CAN 5v Sensors with a Type of Custom 5V, the voltage scale will be 0-5V and the temperature scale will be 0-212 F.
+Use the CAN IDs and input numbers from the list below.
+* Can ID 1575
+** Input 5 = Driver Front
+** Input 6 = Passenger Front
+** Input 7 = Driver Rear
+** Input 8 = Passenger Rear
+
+### For Battery
+Configure as CAN 5V Sensors with a Type of Custom 5V, the voltage scale will be 0-5V and the scale will be 0-100%.
+Use the CAN IDs and input numbers from the list below.
+* Can ID 1576
+** Input 1 = Driver Front
+** Input 2 = Passenger Front
+** Input 3 = Driver Rear
+** Input 4 = Passenger Rear
+
--- tpms.py
+++ tpms.py
@@ -28,30 +28,32 @@ from systemd.journal import JournalHandler
 # Input 3 = Driver Rear
 # Input 4 = Passenger Rear
 
+# Define devices dictionary
+# Each key should be the ID of the sensor in ##:##:##:##:##:## format.
 devices_dict = {
         "80:EA:CA:50:2E:D8": {
-            "data": "80eaca502ed8580903001c0700005f00", 
+            "data": "", 
             "press_canid": 0x1E202627, 
             "temp_canid": 0x1E212627,
             "batt_canid": 0x1E202628,
             "location": "driver_front"
             },
         "81:EA:CA:50:2E:61": {
-            "data": "80eaca502ed8580903001c0700005f00", 
+            "data": "", 
             "press_canid": 0x1E206627,
             "temp_canid": 0x1E216627,
             "batt_canid": 0x1E206628,
             "location": "passenger_front"
             },
         "82:EA:CA:50:2D:18": {
-            "data": "80eaca502ed8580903001c0700005f00", 
+            "data": "", 
             "press_canid": 0x1E20A627, 
             "temp_canid": 0x1E21A627,
             "batt_canid": 0x1E20A628,
             "location": "driver_rear"
             },
         "83:EA:CA:50:2D:34": {
-            "data": "80eaca502ed8580903001c0700005f00", 
+            "data": "", 
             "press_canid": 0x1E20E627, 
             "temp_canid": 0x1E21E627,
             "batt_canid": 0x1E20E628,
@@ -59,7 +61,7 @@ devices_dict = {
             }
 }
 
-# Setup logging
+# Setup logging to log to journald
 log = logging.getLogger('tpms')
 log.addHandler(JournalHandler())
 log.setLevel(logging.INFO)
@@ -74,6 +76,7 @@ def hex2int(_HEX):
   _HEX=_Rev.hex()
   return int(_HEX,16)
 
+# Convert value to required scale
 def remap( x, oMin, oMax, nMin, nMax ):
     oldMin = min( oMin, oMax )
     oldMax = max( oMin, oMax )
@@ -83,91 +86,96 @@ def remap( x, oMin, oMax, nMin, nMax ):
     result = portion + newMin
     return result
 
+# Convert floating point to hex.
 def float_to_hex(f):
     return hex(struct.unpack('<I', struct.pack('<f', f))[0])
 
+# Convert a Hex Value to floating point.
 def hex_to_float(f):
     return struct.unpack('!f',bytes.fromhex(f))[0]
 
+# Create Can message
 def create_can_message(canid,data):
+    # Form CAN message 
     return can.Message(arbitration_id=canid,data=data, is_extended_id=True)
 
+# Convert hex string into a byte array for use in a CAN message
 def create_dlc(x):
+    # Create byte array from string
     return [int('0x'+x[2]+x[3], 16),int('0x'+x[4]+x[5], 16),int('0x'+x[6]+x[7], 16),int('0x'+x[8]+x[9], 16)]
 
+# Send CAN message and bounce interface if it fails
 def send_msg(msg):
     try:
         bus.send(msg)
         return True
     except Exception as e:
-        log.info(e)
+        log.error(e)
         # Bouncing Can network.
         bounce_interface()
 
+# Bounce Interfaces
 def bounce_interface():
     try:
-        log.info("Bouncing Can interface due to error")
+        # Log bounce
+        log.error("Bouncing Can interface due to error")
+        # Bounce Interface
         os.system("ifdown can0")
         os.system("ifup can0")
         time.sleep(.5)
+        # Recreate bus
         global bus 
         bus = can.interface.Bus(channel='can0', interface='socketcan', receive_own_messages=True)
-        #bus.flush_tx_buffer()
         return True
     except Exception as e:
-        log.info(e)
-    
+        log.error(e)
+# End sub ruitines
+
+
 async def main(devices_dict):
     while True:
+        # Scan bluetooth for 4 seconds
         devices = await BleakScanner.discover(return_adv=True,timeout=4)
+        # Iterate over devices from scan looking for the known tpms sensors
         for device in devices.values():
+            # Pull identity and data from device
             identity = device[0].address
             data = device[1].manufacturer_data
+            # If identity isn't a known sensor skip processing.
             if identity not in devices_dict.keys():
                 continue
             else:
-                for key in data:
-                    man_data = data[key].hex()
-                    #print(man_data)
-                    #print(len(man_data))
-                    if len(man_data) == 32:
-                        devices_dict[identity]["data"] = man_data
-                    # Get the proper bytes for each portion
-                    pressure = devices_dict[identity]['data'][12:18]
-                    print(pressure)
-                    print(devices_dict[identity]['data'])
-                    temp = devices_dict[identity]['data'][20:24]
-                    bat = devices_dict[identity]['data'][28:30]
+                man_data = data[0x0100].hex()
+                if len(man_data) == 32:
+                    devices_dict[identity]["data"] = man_data
+                # Get the proper bytes for each portion
+                pressure = devices_dict[identity]['data'][12:18]
+                temp = devices_dict[identity]['data'][20:24]
+                bat = devices_dict[identity]['data'][28:30]
                     
-                    # Convert bytes into floats in proper format
-                    pressurePSI = round((hex2int(pressure)/100000)*14.5037738,2)
-                    tempF = round((hex2int(temp)/100)*1.8 +32,2)
-                    batt = hex2int(bat)
+                # Convert bytes into floats in proper format
+                pressurePSI = round((hex2int(pressure)/100000)*14.5037738,2)
+                tempF = round((hex2int(temp)/100)*1.8 +32,2)
+                batt = hex2int(bat)
                     
-                    #print(pressurePSI)
-                    #print(tempF)
-                    #print(batt)
+                # Remap to 5v for sending to Holley ECU
+                remapped_press = remap(pressurePSI, 0, 50, 0, 5)
+                remapped_temp = remap(tempF, 0, 212, 0, 5)
+                remapped_batt = remap(batt, 0, 100, 0, 5)
                     
-                    # Remap to 5v for sending to Holley ECU
-                    remapped_press = remap(pressurePSI, 0, 50, 0, 5)
-                    remapped_temp = remap(tempF, 0, 212, 0, 5)
-                    remapped_batt = remap(batt, 0, 100, 0, 5)
-                    
-                    # Create Messages
-                    press_msg = create_can_message(devices_dict[identity]["press_canid"],create_dlc(float_to_hex(remapped_press)))
-                    temp_msg = create_can_message(devices_dict[identity]["temp_canid"],create_dlc(float_to_hex(remapped_temp)))
-                    batt_msg = create_can_message(devices_dict[identity]["batt_canid"],create_dlc(float_to_hex(remapped_batt)))
+                # Create Messages
+                press_msg = create_can_message(devices_dict[identity]["press_canid"],create_dlc(float_to_hex(remapped_press)))
+                temp_msg = create_can_message(devices_dict[identity]["temp_canid"],create_dlc(float_to_hex(remapped_temp)))
+                batt_msg = create_can_message(devices_dict[identity]["batt_canid"],create_dlc(float_to_hex(remapped_batt)))
 
-                    # Send Messages
-                    log.info(f"Sending Pressure {pressurePSI} for device {devices_dict[identity]['location']} with voltage {remapped_press}")
-                    send_msg(press_msg)
-                    log.info(f"Sending Temperature {tempF} for device {devices_dict[identity]['location']} with voltage {remapped_temp}")
-                    send_msg(temp_msg)
-                    log.info(f"Sending Battery percentage {batt} for device {devices_dict[identity]['location']} with voltage {remapped_batt}")
-                    send_msg(batt_msg)
+                # Send Messages
+                log.info(f"Sending Pressure {pressurePSI} for device {devices_dict[identity]['location']} with voltage {remapped_press}")
+                send_msg(press_msg)
+                log.info(f"Sending Temperature {tempF} for device {devices_dict[identity]['location']} with voltage {remapped_temp}")
+                send_msg(temp_msg)
+                log.info(f"Sending Battery percentage {batt} for device {devices_dict[identity]['location']} with voltage {remapped_batt}")
+                send_msg(batt_msg)
                     
-                    # Send Messages
-            #print("End Run")
 
 if __name__ == "__main__":
     try:
